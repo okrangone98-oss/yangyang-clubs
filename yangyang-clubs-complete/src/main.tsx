d32473c76cbd9173a4868c7ea45f30f083b7e501
david38 <okrangone98@gmail.com>
2025-08-13 19:16:56 +0900

    Update main.tsx

--- yangyang-clubs-complete/src/main.tsx
+++ yangyang-clubs-complete/src/main.tsx
@@ -11,15 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </HashRouter>
   </React.StrictMode>
 );
-import InstructorRegister from "./components/InstructorRegister";
-
-function App() {
-  return (
-    <div>
-      <h1>강사 등록 시스템</h1>
-      <InstructorRegister />
-    </div>
-  );
-}
-
-export default App;
