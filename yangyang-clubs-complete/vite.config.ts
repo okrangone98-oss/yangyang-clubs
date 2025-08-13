06df15e4879e6d6cdc27e30370b68d994cfe5ef8
david38 <okrangone98@gmail.com>
2025-08-13 16:55:11 +0900

    Update vite.config.ts

--- yangyang-clubs-complete/vite.config.ts
+++ yangyang-clubs-complete/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/yangyang-clubs/',   // 리포지토리명이 다르면 '/리포명/' 으로 변경
+  base: '/yangyang-clubs/', // 리포명과 동일
 })
