72614b66c820fd329a020d7c4c4d271de26bcdbe
david38 <okrangone98@gmail.com>
2025-08-13 14:37:39 +0900

    Update main.tsx

--- yangyang-clubs-complete/src/main.tsx
+++ yangyang-clubs-complete/src/main.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App'   // 대문자 A
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
