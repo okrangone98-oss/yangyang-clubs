c6c4b13277fc7a48288663b0d1d3c15dd679d1b8
david38 <okrangone98@gmail.com>
2025-08-13 16:49:37 +0900

    Update main.tsx

--- yangyang-clubs-complete/src/main.tsx
+++ yangyang-clubs-complete/src/main.tsx
@@ -1,3 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'   // ← 여기서만 불러옵니다
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+
+
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
