cd0e5910c8ceca277c3661ae5febc5bc798c2c29
david38 <okrangone98@gmail.com>
2025-08-13 16:16:16 +0900

    Update main.tsx

--- yangyang-clubs-complete/src/main.tsx
+++ yangyang-clubs-complete/src/main.tsx
@@ -1,11 +1,15 @@
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'   // 대문자 A
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* GH Pages에 맞춰 베이스 자동 적용 */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
